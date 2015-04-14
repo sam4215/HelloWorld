4b51201bedf3043d031751407ca75c8211f3fb2a
TheDoctorsLife <hoffmeyer25@gmail.com>
2015-04-14 12:57:08 -0400

    Intial push to github
    Signed-off-by: TheDoctorsLife <hoffmeyer25@gmail.com>

---
+++ src/src/thedoctorslife/com/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+* @author benjamin.hoffmeyer
+*/
+// package
+package src.thedoctorslife.com;
+
+// Import Hello and Cheese
+public class HelloWorld  {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)  {
+    	printHello();
+    	printCheese();
+    }
+}
---
+++ src/src/thedoctorslife/com/Stuff.java
@@ -0,0 +1,20 @@
+/**
+ * @author benjamin.hoffmeyer
+ *
+ */
+// package
+package src.thedoctorslife.com;
+
+
+// Define the class
+public class Stuff extends HelloWorld {
+
+		// Define printHello and execute
+		private static void printHello() {
+			System.out.println("Hello World!");
+			}
+		// Define printCheese and execute.
+		private static void printCheese() {
+			System.out.println("I like cheese!");
+			}
+}
