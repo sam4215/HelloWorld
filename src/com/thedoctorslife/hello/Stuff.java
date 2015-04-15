a6f108a2756d5e52fde257ab7704a6f3a920426d
sam4215 <sam4215@users.noreply.github.com>
2015-04-15 15:08:23 -0500

    camelCase methods

--- src/com/thedoctorslife/hello/Stuff.java
+++ src/com/thedoctorslife/hello/Stuff.java
@@ -9,20 +9,20 @@ package com.thedoctorslife.hello;
 public class Stuff {
 
 		// Define printHello and execute
-		public static void Hello() {
+		public static void hello() {
 			System.out.print("Hello World, ");
 			}
 		// Define printCheese and execute.
-		public static void Cheese() {
+		public static void cheese() {
 			System.out.println("I like cheese!");
 			}
 		// Make a funny
-		public static void End() {
+		public static void end() {
 			System.out.println("The end.");
 			System.exit(0);
 		}
 		// Make a real funny
-		public static void End2() {
+		public static void end2() {
 			System.out.print("The end....");
 			System.out.println("Or is it?");
 		}
